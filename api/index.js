20c7a38bca6a732d30056fe4fc3dadb2264ee071
Emmanuel Ejim <emmanueleejim@gmail.com>
2021-04-22 15:49:39 -0500

    new Routes and async functions for Login

--- api/index.js
+++ api/index.js
@@ -1,36 +1,38 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+// Before we start attaching our routers
+const jwt = require('jsonwebtoken');
+const { getUserById } = require('../db');
+const { JWT_SECRET } = process.env;
+
+// set `req.user` if possible
 apiRouter.use(async (req, res, next) => {
-   const prefix = 'Bearer '
-   const auth = req.headers['Authorization'];
- 
-   if (!auth) {
-     next(); // don't set req.user, no token was passed in
-   }
- 
-   if (auth.startsWith(prefix)) {
-     // recover the token
-     const token = auth.slice(prefix.length);
-     try {
-       // recover the data
-       const { id } = jwt.verify(data, 'secret message');
- 
-       // get the user from the database
-       const user = await getUserById(id);
-       // note: this might be a user or it might be null depending on if it exists
- 
-       // attach the user and move on
-       req.user = user;
- 
-       next();
-     } catch (error) {
-        res.send({error})
-        console.log(error)
-       // there are a few types of errors here
-     }
-   }
- })
+   const auth = req.header('Authorization');
+   const prefix = 'Bearer ';
+
+  if (!auth) { // nothing to see here
+    next();
+  } else if (auth.startsWith(prefix)) {
+    const token = auth.slice(prefix.length);
+
+    try {
+      const { id } = jwt.verify(token, JWT_SECRET);
+
+      if (id) {
+        req.user = await getUserById(id);
+        next();
+      }
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  } else {
+    next({
+      name: 'AuthorizationHeaderError',
+      message: `Authorization token must start with ${ prefix }`
+    });
+  }
+});
 
 const usersRouter = require('./users');
 apiRouter.use('/users', usersRouter);
@@ -41,6 +43,9 @@ apiRouter.use('/posts', userPosts)
 const userTags = require('./tags');
 apiRouter.use('/tags', userTags);
 
-
+//-----Error Handler-----
+apiRouter.use((error, req, res, next) => {
+   res.send(error);
+});
 
 module.exports = apiRouter;
