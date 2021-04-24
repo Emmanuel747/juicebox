f8c512e0550587c3fbfd5df95dfc51252bd288b2
Emmanuel Ejim <emmanueleejim@gmail.com>
2021-04-24 00:46:08 -0500

    added new req.params varible to store URL Nums

--- index.js
+++ index.js
@@ -21,6 +21,11 @@ server.use((req, res, next) => {
 const apiRouter = require('./api');
 server.use('/api', apiRouter);
 
+server.get('/add/:first/to/:second', (req, res, next) => {
+   res.send(`<h1>${ req.params.first } + ${ req.params.second } = ${
+     Number(req.params.first) + Number(req.params.second)
+    }</h1>`);
+ });
 
 server.listen(PORT, () => {
    console.log('The server is up on port', PORT)
