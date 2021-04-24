8ebef2569e63c56702717b2e119eb18cbaf14609
Emmanuel Ejim <emmanueleejim@gmail.com>
2021-04-24 09:13:14 -0500

    cleaned up DB message resposne

--- db/index.js
+++ db/index.js
@@ -267,17 +267,20 @@ async function addTagsToPost(postId, tagList) {
 
 async function getPostById(postId) {
   try {
-    const {
-      rows: [post],
-    } = await client.query(
-      `
+    const { rows: [ post ]  } = await client.query(`
       SELECT *
       FROM posts
       WHERE id=$1;
-    `,
-      [postId]
-    );
+    `, [postId]);
 
+    // THIS IS NEW
+    if (!post) {
+      throw {
+        name: "PostNotFoundError",
+        message: "Could not find a post with that postId"
+      };
+    }
+    
     const { rows: tags } = await client.query(
       `
       SELECT tags.*
