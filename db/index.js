b2e9ece1ddd551aa9ce3d55876bf394e5d15e4dd
Emmanuel Ejim <emmanueleejim@gmail.com>
2021-04-25 15:36:48 -0500

    heroku connection update

--- db/index.js
+++ db/index.js
@@ -1,6 +1,10 @@
 //Imports the pg module
 const { Client } = require("pg"); 
-const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev');
+//Heroku SSL connection ternary
+const client = new Client({
+  connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev',
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+});
 
 //USER Methods
 async function createUser({ username, password, name, location }) {
